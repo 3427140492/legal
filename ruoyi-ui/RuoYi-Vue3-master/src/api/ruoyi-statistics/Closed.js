68f3d7bbe51f9e49398a57da9f77f41ffb205aa1
shen <3539013756@qq.com>
2022-09-20 20:05:02 +0800

    项目初始化

--- ruoyi-ui/RuoYi-Vue3-master/src/api/ruoyi-statistics/Closed.js
+++ ruoyi-ui/RuoYi-Vue3-master/src/api/ruoyi-statistics/Closed.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 // 查询收结案统计列表
 export function listClosed(query) {
   return request({
-    url: '/lvshi/caselaw/list',
+    url: '/ruoyi-statistics/Closed/list',
     method: 'get',
     params: query
   })
@@ -12,7 +12,7 @@ export function listClosed(query) {
 // 查询收结案统计详细
 export function getClosed(id) {
   return request({
-    url: '/lvshi/caselaw/' + id,
+    url: '/ruoyi-statistics/Closed/' + id,
     method: 'get'
   })
 }
@@ -20,7 +20,7 @@ export function getClosed(id) {
 // 新增收结案统计
 export function addClosed(data) {
   return request({
-    url: '/lvshi/caselaw',
+    url: '/ruoyi-statistics/Closed',
     method: 'post',
     data: data
   })
@@ -29,7 +29,7 @@ export function addClosed(data) {
 // 修改收结案统计
 export function updateClosed(data) {
   return request({
-    url: '/lvshi/caselaw',
+    url: '/ruoyi-statistics/Closed',
     method: 'put',
     data: data
   })
@@ -38,7 +38,7 @@ export function updateClosed(data) {
 // 删除收结案统计
 export function delClosed(id) {
   return request({
-    url: '/lvshi/caselaw/' + id,
+    url: '/ruoyi-statistics/Closed/' + id,
     method: 'delete'
   })
 }
